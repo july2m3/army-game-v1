52978cba01257bc2d7d46733189a30656cf7920f
Julio <july2m3@gmail.com>
2020-06-26 11:09:18 -0500

    Updates

---
+++ precache-manifest.deb7e97db60fd21237521d6584b8f81c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1cd798bc5fdc890f417141d32b8f88",
+    "url": "/army-game-v1/index.html"
+  },
+  {
+    "revision": "733af5af0039de92a637",
+    "url": "/army-game-v1/static/css/main.fbf7780c.chunk.css"
+  },
+  {
+    "revision": "665904cdcf3be63da582",
+    "url": "/army-game-v1/static/js/2.e30c81f8.chunk.js"
+  },
+  {
+    "revision": "ed901023237d66f16963aae8c7889084",
+    "url": "/army-game-v1/static/js/2.e30c81f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733af5af0039de92a637",
+    "url": "/army-game-v1/static/js/main.6c1101e1.chunk.js"
+  },
+  {
+    "revision": "db2f0c9f795fcd198348",
+    "url": "/army-game-v1/static/js/runtime-main.3a89b12a.js"
+  },
+  {
+    "revision": "adc77528829978b6867d69493ff04252",
+    "url": "/army-game-v1/static/media/stage.adc77528.jpeg"
+  }
+]);
