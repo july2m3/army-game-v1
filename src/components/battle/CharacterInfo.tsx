1f1b774bc8f638fb712116075181b963866dfe63
Julio <july2m3@gmail.com>
2020-06-26 13:20:39 -0500

    HP bars move upon sass variables

--- src/components/battle/CharacterInfo.tsx
+++ src/components/battle/CharacterInfo.tsx
@@ -5,30 +5,8 @@ const CharacterInfo = (props: any) => {
 
   return (
     <div className="character-info">
-      <ul>
-        <li className="character-info__name">{currentCharacter.name}</li>
-        {/* <li className="character-info__hp">
-          {currentCharacter.hp}
-          HP{' '}
-          <span role="img" aria-label="HP">
-            🛡
-          </span>
-        </li> */}
-        {/* 
-        <li>
-          {currentCharacter.attack}
-          attack{' '}
-          <span role="img" aria-label="Attack">
-            ⚔️
-          </span>
-        </li>
-        <li>
-          {currentCharacter.accuracy}% Accuracy
-          <span role="img" aria-label="Accuracy">
-            🎯
-          </span>
-        </li> */}
-      </ul>
+      {/* <span className="character-info__hp"></span> */}
+      <p className="character-info__name">{currentCharacter.name}</p>
     </div>
   );
 };
